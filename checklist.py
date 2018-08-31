7a57f1fde3af76faf7fe66a97eef1a0a0e07fce3
Nolan Kovacik <noltron000@gmail.com>
2018-08-31 14:25:38 -0700

    fleshing out checklist.py

--- checklist.py
+++ checklist.py
@@ -1,15 +1,36 @@
-print("Hello World")
+# Lets make some noise :D
+# We'll start with printing and functions.
+print()
+
+print("<=== Hello World ===>")
+print("Welcome to THE CHECKLIST")
 
 def my_fun_function(say_this):
 	print(say_this)
 
 my_fun_function('Hello World')
 
+# Lets run through making a list.
+print()
+
+print("we are going to make some modifications to a list...")
 checklist = []
+print(checklist)
 checklist.append('Blue')
 print(checklist)
 checklist.append('Orange')
 print(checklist)
+checklist = ['Hello', 'World']
+print(checklist)
+checklist[1] = 'Cats'
+print(checklist)
+checklist = ['Hello', 'World']
+print(checklist)
+checklist.pop(1)
+print(checklist)
+
+# Now, lets sum it up within FUNCTIONS.
+print()
 
 def create(item):
 	checklist.append(item)
@@ -17,27 +38,17 @@ def create(item):
 def read(index):
 	return checklist[index]
 
-checklist = ['Hello', 'World']
-checklist[1] = 'Cats'
-print(checklist)
-
 def update(index, item):
 	checklist[index] = item
-
-checklist = ['Hello', 'World']
-checklist.pop(1)
-print(checklist)
 
 def destroy(index):
 	checklist.pop(index)
 
-
 def list_all_items():
-    index = 0
-    for list_item in checklist:
-        print(str(index) + list_item)
-        index += 1
-
+	index = 0
+	for list_item in checklist:
+		print(str(index) + ': ' + list_item)
+		index += 1
 
 def test():
 	create("purple sox")
